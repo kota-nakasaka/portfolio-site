c0de16d8ea29eebae7a80d70a971eb85c2e00f6f
kota-nakasaka <kodai.photography1217@gmail.com>
2021-12-11 12:50:57 +0900

    main

--- js/test.js
+++ js/test.js
@@ -2,12 +2,12 @@ $(function () {
     $(".slider").slick({
       arrows: false,
       autoplay: true,
-      autoplaySpeed: 3000,
+      autoplaySpeed: 2000,
       centerMode: true,
       centerPadding: "30px",
       dots: true,
       // fade: true,
-      slidesToShow: 3,
+      slidesToShow: 2,
       speed: 1000,
     });
   });
